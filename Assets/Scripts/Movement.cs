9af87d34da2a2e17f015b0c72ef766de1e31eac2
Morialle Barr <Moriallekbarr@gmail.com>
2025-07-02 12:34:33 +0300

    Lessons 4-6

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,16 +1,42 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] InputAction thrust;
+    [SerializeField] InputAction rotation;
+    [SerializeField] float thrustStrength = 100f;
+
+    private void OnEnable()
     {
-        
+        thrust.Enable();
+        rotation.Enable();
     }
 
-    // Update is called once per frame
-    void Update()
+    Rigidbody rb;
+
+    void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    private void ProcessThrust()
+    {
+        if (thrust.IsPressed())
+        {
+            rb.AddRelativeForce(Vector3.up * thrustStrength * Time.fixedDeltaTime);
+        }
+    }
+
+    private void ProcessRotation()
+    {
+        float rotationInput = rotation.ReadValue<float>();
+        Debug.Log("here is our rotatoion value: " + rotationInput);
     }
 }
